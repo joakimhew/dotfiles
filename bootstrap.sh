b77b9135bff21aeb9858281fa7f20066bc53c94c
Joakim Hansson <joakimhew@gmail.com>
2022-07-25 23:56:04 +0200

    First update

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,30 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
 
+# TODO: Add script for installing oh my zsh, iTerm2 etc.
+# function installZshPlugins() {
+# 	if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+# 		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# 	fi
+# }
+
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
+		--exclude ".bash_prompt" \
+		--exclude ".bash_profile" \
+		--exclude ".bashrc" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	source $ZSH/oh-my-zsh.sh;
+	exec /bin/zsh;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
+unset installZshPlugins;
 unset doIt;
+
+installZshPlugins;
+doIt;
